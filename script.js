99ffebeab88da9b7395e2f529d533084e3fa50ce
yotamfogelman <38930122+yotamfogelman@users.noreply.github.com>
2018-05-03 10:07:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// Change text in an element
-// #("h1").text("New header!");
-
-// Retrieve or change the HTML of an element/s
-// $("ul").html();
-// $("ul").html("<li>Item 1</li><li>Item 2</li>");
-
-// Change an element's attribute
-// $("input").attr("type", "password");
-
-// Get/change an input's value (or a select)
-// $("input").val();
-// $("input").val("New Input Value");
-
-// Add, remove, or toggle class of an element
-// ("h1").addClass("class1");
-// ("h1").removeClass("class1");
-// ("h1").toggleClass("class1");
-
+$("button").click(function () {
+	alert("Just kidding, check your email for your present!");
+});
