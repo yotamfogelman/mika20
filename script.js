6b7b327fa07f0d0bee0a1b9044a7409fe6c85dac
yotamfogelman <38930122+yotamfogelman@users.noreply.github.com>
2018-05-03 10:12:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$("button").click(function () {
+// $("button").click(function () {
+// 	alert("Just kidding, check your email for your present!");
+// });
+
+var pBtn = document.querySelector("button");
+
+pBtn.addEventListener("click", function() {
 	alert("Just kidding, check your email for your present!");
 });
